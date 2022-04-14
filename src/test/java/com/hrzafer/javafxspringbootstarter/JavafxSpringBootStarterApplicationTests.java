f112fe73397af138d99d2d04dd605fbfd03940d1
Harun Zafer <harunzafer@gmail.com>
2022-04-14 01:06:03 -0400

    Initial Spring Boot Project

---
+++ src/test/java/com/hrzafer/javafxspringbootstarter/JavafxSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrzafer.javafxspringbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavafxSpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
